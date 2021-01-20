60e0aa09a2aef84372c2b052e1137f2e5ca2dc4f
KietChan <kiet.ly@unified.com>
2021-01-20 20:44:43 +0700

    trigger build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MarketingApp from "./components/MarketingApp";
-// .... test push event ...
+// .... test push event ....
 export default () => {
     return <div>
         <h1>Hi There!</h1>
